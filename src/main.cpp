1925356f379474f78aa6b0709a1dbaacfe76cab2
ben <bnoack@techshot.com>
2020-08-26 09:22:59 -0400

    tabs

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,15 @@
 
 int main(void)
 {
-    SystemInit();
+	SystemInit();
 
-    REG_PORT_DIR0 |= LED_PIN;//set the direction to output
-    REG_PORT_OUT0 |= LED_PIN;//set the pin high (LED off)
+	REG_PORT_DIR0 |= LED_PIN;//set the direction to output
+	REG_PORT_OUT0 |= LED_PIN;//set the pin high (LED off)
 
-    while (1) 
-    {
-	    for (uint32_t i = 0; i < 100000; i++)
-		    ;
+	while (1) {
+		for (uint32_t i = 0; i < 100000; i++);
 
-    	REG_PORT_OUTTGL0 |= LED_PIN;
-    }
+		REG_PORT_OUTTGL0 |= LED_PIN;
+	}
 }
 
